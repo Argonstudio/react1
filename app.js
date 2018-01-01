9d1a6fc8fa9cfbb9d127c391a6de9db042145722
/usr/bin/vim <support@beget.ru>
2018-01-01 15:50:26 +0300

    1

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var server = http.createServer(function(req, res) {
 //touch tmp/restart.txt
 //npm run build
 
+//git add .
+//git commit -m "..."
+//git push /pull
+
 if( req.url == "/" ){
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
